240d09208f3ac383ec0d9562ff30cb7d076aec92
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-28 22:42:53 +0530

    initial

---
+++ security/SpringWebSecurityTL2/src/main/java/com/laxtech/example/SpringWebSecurityTl2Application.java
@@ -0,0 +1,12 @@
+package com.laxtech.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityTl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityTl2Application.class, args);
+	}
+}
