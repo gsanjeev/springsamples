9064e727d4cb81e0f312d0f9d3dbe3b6f33e78e8
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-28 15:44:26 +0530

    initial

---
+++ security/SpringWebSecurityTL/src/main/java/com/laxtech/example/SpringWebSecurityTlApplication.java
@@ -0,0 +1,12 @@
+package com.laxtech.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityTlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityTlApplication.class, args);
+	}
+}
