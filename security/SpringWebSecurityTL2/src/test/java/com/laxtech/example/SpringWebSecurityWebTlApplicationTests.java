e35fbe6da1a3c2c7840390ee2c236a6b7226443e
Sanjeev <gsanjeevtripathi@gmail.com>
2017-10-28 22:44:57 +0530

    initial

---
+++ security/SpringWebSecurityTL2/src/test/java/com/laxtech/example/SpringWebSecurityWebTlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laxtech.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebSecurityWebTlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
